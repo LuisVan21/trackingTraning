bf1679737fc93765aacc5ca00cd8e394db47c812
luis garcia <fisic_26@hotmail.com>
2022-11-04 16:40:07 -0700

    created database

---
+++ tracking_DATABASE.sql
@@ -0,0 +1,11 @@
+use trainingTracking;
+CREATE TABLE clientT(
+ID_client INT (12) NOT NULL auto_increment,
+FirstName varchar(30) not null,
+LastName varchar(30),
+Age int (10),
+Password1 int(20),
+text1 text(2000),
+primary key(ID_client)
+);
+
